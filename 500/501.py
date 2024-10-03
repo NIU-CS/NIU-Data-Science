c8f7b49387b437f509efcfa70628e07a498adf90
Lcd0327 <lcd0327@gmail.com>
2024-10-03 14:34:58 +0800

    501~510

---
+++ 500/501.py
@@ -0,0 +1,11 @@
+#501
+def compute():
+    department = input("Department: ")
+    student_id = input("Student ID: ")
+    name = input("Name: ")
+
+    print(f"Department: {department}")
+    print(f"Student ID: {student_id}")
+    print(f"Name: {name}")
+
+compute()
