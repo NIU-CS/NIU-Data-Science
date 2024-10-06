569cdd2873f7161ccc13c9eac9b7fc5b3440b9b3
Lcd0327 <lcd0327@gmail.com>
2024-10-06 14:01:49 +0800

    601-602

---
+++ 600/602.py
@@ -0,0 +1,13 @@
+L=[]
+for i in range(5):
+  x = input()
+  if x == 'A':
+    x = 1
+  if x == 'J':
+    x = 11
+  if x == 'Q':
+    x = 12
+  if x == 'K':
+    x = 613
+  L.append(int(x))
+print(sum(L))
