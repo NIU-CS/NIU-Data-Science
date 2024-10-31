685db6c54b8ad5b1b46ea8fbf337db45b67eb322
Lcd0327 <lcd0327@gmail.com>
2024-10-31 18:41:49 +0800

    901~903

---
+++ 900/902.py
@@ -0,0 +1,13 @@
+#902
+
+def main():
+    total = 0
+    with open("read.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+        numbers = map(int, content.split())
+        total = sum(numbers)
+    
+    print(f"{total}")
+
+if __name__ == "__main__":
+    main()
---
+++ 900/903.py
@@ -0,0 +1,14 @@
+#903
+
+def main():
+    with open("data.txt", "a", encoding="utf-8") as file:
+        for i in range(5):
+            name = input()
+            file.write(name + "\n")
+
+    with open("data.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
+
+if __name__ == "__main__":
+    main()
